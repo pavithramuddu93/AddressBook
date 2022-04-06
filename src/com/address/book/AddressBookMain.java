3010f91f3009e9be2332b30452878885251dbb1c
pavithramuddu93 <pavithramuddu93>
2022-04-06 10:16:03 +0530

    UC7 added

--- src/com/address/book/AddressBookMain.java
+++ src/com/address/book/AddressBookMain.java
@@ -1,31 +1,136 @@
 package com.address.book;
+
+import com.address.book.Person;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Program");
-        Scanner in = new Scanner(System.in);
-        AddressBook contactInfo = new AddressBook();
-        contactInfo.setUpInfo();
-        contactInfo.displayListItems();
-        int i;
-        int j = 0;
-        for (i = 1; i > j; i++) {
-            System.out.println("Press 1 to continue creating Address Book/Press 2 to edit a contact in an Address Book/Press 3 to delete a contact from an Address Book/Press 4 to Add a New Contact to an Address Book/Press 0 to exit the program");
-            int operation = in.nextInt();
-            if (operation == 1) {
-                contactInfo.setUpInfo();
-                contactInfo.displayListItems();
-            } else if (operation == 2) {
-                contactInfo.editContact();
+    private static Scanner scanner = new Scanner(System.in);
+    private static Map<String, Person> personMap = new HashMap();
+    private static Map<String, Map<String, Person>> addressBookMap = new HashMap();
 
-            } else if (operation == 0) {
-                j = i + 2;
-            } else if (operation == 3) {
-                contactInfo.deleteContact();
-            } else if (operation == 4) {
-                contactInfo.add();
+    /**
+     * This is the main method, Here is the starting point of the program.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        boolean isExit = false;
+
+        do {
+            System.out.println("\n\t\tAddress Book System");
+            System.out.println("\n\t\tEnter A to Add Person ");
+            System.out.println("\t\tEnter E to Edit Person");
+            System.out.println("\t\tEnter D to Delete Person");
+            System.out.println("\t\tEnter S to Show Person Detail");
+            System.out.println("\t\tEnter Q to Quit ");
+            System.out.print("\t\tPlease Select One Option : ");
+            char userInput = scanner.nextLine().toUpperCase().charAt(0);
+            switch (userInput) {
+                case 'A':
+                    //add
+                    addBook();
+                    break;
+                case 'E':
+                    //edit
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String cityName = scanner.nextLine();
+                    editContact(firstName,cityName);
+                    break;
+                case 'D':
+                    //delete
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String personName = scanner.nextLine();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String city = scanner.nextLine();
+                    deletePerson(personName,city);
+                    break;
+                case 'S':
+                    //Show
+                    System.out.println("\n\t\t" + addressBookMap.toString());
+                    break;
+                case 'Q':
+                    //quit
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Please select correct option");
             }
+        } while (!isExit);
+
+    }
+
+
+    /**
+     * Method For editing the existing person details on the basis of first name.
+     */
+    private static void editContact(String firstName, String cityName) {
+        personMap = addressBookMap.get(cityName);
+        System.out.println(personMap.toString());
+        if (addressBookMap.get(cityName).get(firstName) != null) {
+            Person editedPerson = contactFields();
+            personMap.put(editedPerson.getFirstName(), editedPerson);
+            addressBookMap.put(editedPerson.getCity(), personMap);
+        } else {
+            System.out.println("Record Not Found");
         }
+        System.out.println("\n\t\t" + addressBookMap.toString());
+    }
+
+
+    private static Person contactFields() {
+        Person person = new Person();
+        System.out.print("Enter First Name : ");
+        person.setFirstName(scanner.nextLine());
+
+        System.out.print("Enter Last Name : ");
+        person.setLastName(scanner.nextLine());
+
+        System.out.print("Enter Address : ");
+        person.setAddress(scanner.nextLine());
+
+        System.out.print("Enter State : ");
+        person.setState(scanner.nextLine());
+
+        System.out.print("Enter City : ");
+        person.setCity(scanner.nextLine());
+
+        System.out.print("Enter ZipCode : ");
+        person.setZip(scanner.nextLine());
+
+        System.out.print("Enter Phone Number : ");
+        person.setPhone(scanner.nextLine());
+        return person;
+    }
+
+    private static void deletePerson(String firstName, String cityName) {
+        Person newPerson = addressBookMap.get(cityName).get(firstName);
+        System.out.println(newPerson.toString());
+        if (addressBookMap.get(cityName).get(firstName) != null) {
+            addressBookMap.get(cityName).remove(firstName);
+            System.out.println("Deleted Successfully");
+        } else {
+            System.out.println("Record not exist");
+        }
+
+    }
+
+    private static void addBook() {
+        Map<String, Person> newPersonMap = new HashMap();
+        Person newPerson;
+        newPerson = contactFields();
+
+        if (addressBookMap.get(newPerson.getCity()) != null)
+            newPersonMap = addressBookMap.get(newPerson.getCity());
+
+        newPersonMap.put(newPerson.getFirstName(), newPerson);
+        addressBookMap.put(newPerson.getCity(), newPersonMap);
+
+        System.out.println("\n\t\t" + addressBookMap.toString());
     }
 }
